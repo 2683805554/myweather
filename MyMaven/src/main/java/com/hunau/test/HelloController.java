7ffd408b01fa1edab336fca3b9b1d8f3d7318147
2683805554 <2683805554@qq.com>
2019-09-30 14:38:54 +0800

    first commit

---
+++ MyMaven/src/main/java/com/hunau/test/HelloController.java
@@ -0,0 +1,17 @@
+package com.hunau.test;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "王林彦，学号201841882127!";
+    }
+}
